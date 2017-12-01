97686d90519e2d8ee73f65c2901f284215030001
Stephen Akinyooye <galvatron@Stephens-MacBook-Pro.local>
2017-12-01 12:31:55 -0500

    Integrated Event invite form with corresponding route/controller

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -4,55 +4,50 @@ const yelpSearch = require('./yelpController.js').yelpSearch;
 let eventRef = dbRef.child('event');
 
 exports.createEvent = function(req, res){
-    console.log(req.body)
+    console.log(req.body);
     //object to be constructed from request object
-    var searchRequestParams = {
+    let searchRequestParams = {
         limit: 20,
         sort_by: 'best_match',
-        location: 'crown heights',
+        location: req.body.address,
         locale: 'en_US',
         radius: 6437,//This value corresponds with 4 miles (in meters). The default value is 10000 meters(6.2 miles)
-        term: 'pizza'
+        term: req.body.searchTerm
         //longitude
         //latitude
         //categories:
     };
 
-    yelpSearch(searchRequestParams, new Date('Thu Dec 21 2017 18:00:00 GMT-0500 (EST)')). then(yelpSearchResultsKey => {
-        eventRef.push({
-            locationDetails: {
-                address: '369 Lexington Ave.',
-                latitude: null,
-                longitude: null
-            },
-            yelpSearchResultsKey: yelpSearchResultsKey,
-            foodType: 'Pizza',
-            hostName: 'Stephen Akinyooye',
-            hostContactType: 'email',
-            hostContactDetails: 'sakinyooye@gmail.com',
-            eventDateTime: 'Thu Dec 21 2017 18:00:00 GMT-0500 (EST)', //'12/21/2017 6:00PM'
-            voteCuteOffDateTime: 'Wed Dec 13 2017 09:00:00 GMT-0500 (EST)', //'12/13/2017 9:00 AM',
-            eventCreationDateTime: 'Wed Nov 29 2017 12:37:48 GMT-0500 (EST)',
-            eventInvitees: [{
-                Name: 'RJ Barnett',
-                contactType: 'email',
-                contactDetails: 'rj@barnett.com',
-                voteCompleted: true
-            }, {
-                Name: 'Jimmy Sanders-Cannon',
-                contactType: 'email',
-                contactDetails: 'jimmy@sanders-cannon.com',
-                voteCompleted: true
-            },
-                {
-                    Name: 'Gil Mandler',
-                    contactType: 'email',
-                    contactDetails: 'gil@mandler.com',
-                    voteCompleted: true
-                }
-            ]
+    let eventDetails = {
+        locationDetails: {
+            address: req.body.address,
+            latitude: null,
+            longitude: null
+        },
+        foodType: req.body.searchTerm,
+        hostName: req.body.hostName,
+        hostContactType: 'email',
+        hostContactDetails: req.body.hostEmail,
+        eventDateTime: new Date(req.body.dateTime).toString(),
+        eventCreationDateTime: new Date().toString(),
+        eventInvitees: [],
+        voteCutOffDateTime: new Date().toString() //new Date(req.body.voteCutOffDateTime).toString()
+    };
+
+    req.body.guestEmails.forEach((guestEmail) => {
+        eventDetails.eventInvitees.push({
+            Name: null,
+            contactType: 'email',
+            contactDetails: guestEmail,
+            voteCompleted: false
         });
     });
 
-    res.send('Event Controller Test');
+    yelpSearch(searchRequestParams, new Date('Thu Dec 21 2017 18:00:00 GMT-0500 (EST)')). then(yelpSearchResultsKey => {
+        eventDetails.yelpSearchResultsKey = yelpSearchResultsKey;
+        let newDataPath = eventRef.push(eventDetails);
+        res.send(newDataPath.key);
+    });
+
+    //res.send('Event Controller Test');
 };
