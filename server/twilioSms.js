af8582bf45c247f628465695f67695a06e1b7240
RJ Barnett <RJ.Barnett1@gmail.com>
2017-12-02 12:02:09 -0500

    comments to auth and twilio files

--- server/twilioSms.js
+++ server/twilioSms.js
@@ -10,7 +10,9 @@ const keys = require('./keys/twiliokeys.js')
 //client requires the account keys saved in the gitignored keys folder
 const client = require('twilio')(keys.accountSid, keys.authToken);
 
-
+//when called this method sends SMS to the desired 'to' number
+//'from' number always stays the same, it is the number registered on twilio acct
+//'to' and 'body' fields can by dynamic
 client.messages.create({
     to: '+18587616610',
     from: "+13473086897",
