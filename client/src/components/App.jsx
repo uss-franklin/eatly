50a2b69eea436c7a96410cb4204873006cf5a777
refactoredjello <gil.mandler@gmail.com>
2017-12-02 10:42:06 -0500

    Refactor router structure - now rout is in in app.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,30 +1,36 @@
 import React from 'react'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+
+import Home from './Home'
+import InputForm from './event_form/InputForm'
+import Swipe from './Swipe'
+import LoginForm from './login/LoginForm'
 import Location from './location_form/Location'
 import NavBar from './NavBar'
-import {Link} from 'react-router-dom'
-
-
-import Swipe from './Swipe'
 
 export default class App extends React.Component {
   constructor(props) {
     super(props)
-  }  
+    this.state = {
+      firebaseAuthenticatedUser: ''
+    }
+  }
+  grabFirebaseUser(firebaseUser){
+    this.setState({firebaseAuthenticatedUser: firebaseUser})
+  }
 
   render() {
     // console.log(this.props)
     return (
-      <div className="parent">
-          <NavBar />
-        <div className="picture">
-          <div className="title"><h1>Welcome to eatly</h1></div>
-          <div className="planMealButton">
-              <button className="mealButton">
-                <Link to="/inputForm" style={{ textDecoration: 'none'}}>Plan a Meal</Link>
-              </button>  
-          </div>
-        </div>
-      </div>
+
+    <Router>
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/LoginForm" component={LoginForm} />
+      <Route exact path="/inputForm" component={InputForm} /> 
+      <Route exact path="/swipe" component={Swipe} />
+    </div>
+  </Router>
     )
   }
 }
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,18 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
 import App from './components/App'
-import InputForm from './components/event_form/InputForm'
-import Swipe from './components/Swipe'
-import LoginForm from './components/login/LoginForm'
 
-ReactDom.render(
-  <Router>
-    <div>
-      <Route exact path="/" component={App} />
-      <Route exact path="/LoginForm" component={LoginForm} />
-      <Route exact path="/inputForm" component={InputForm} /> 
-      <Route exact path="/swipe" component={Swipe} />
-    </div>
-  </Router>,  
-  document.getElementById('app'));
+
+ReactDom.render(<App />,  document.getElementById('app'));
