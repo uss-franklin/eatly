155168333a78f71ff4133f159f3bd0f0bcfe92ee
RJ Barnett <RJ.Barnett1@gmail.com>
2017-12-06 11:55:44 -0500

    fleshing out the editEventController and its editEvent functionality

--- server/controllers/editEventController.js
+++ server/controllers/editEventController.js
@@ -4,9 +4,13 @@ const createGuestEmailUser = require('./userController.js').createGuestEmailUser
 
 const EventsRef = dbRef.child('events');
 
+//pulls in the sent object that's assigned values from the edit event form component
+//this function queries the DB for the event by its name, then sets new values to it
 exports.editEvent = function(req, res) {
-	EventsRef.orderByKey().equalTo(req.body.value).once('child_changed')
+	EventsRef.orderByKey().equalTo(req.body.eventName).once('child_changed')
 	  .then(event => {
+	  	//TODO add in functionality for setting data to DB
+	  	EventRef.set({${thing}: ${other thing}})
 	  	console.log("event edited! resulting changes to db : " + event)
 	  })
 }
