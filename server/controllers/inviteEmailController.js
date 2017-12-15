73f758daed2fb6ae5d8ac0ac54e4ec266439c8c3
RJ Barnett <RJ.Barnett1@gmail.com>
2017-12-15 14:12:15 -0500

    reformatted invite email to match template literals conventions, also to include working links to users special voting page

--- server/controllers/inviteEmailController.js
+++ server/controllers/inviteEmailController.js
@@ -18,11 +18,11 @@ const transporter = nodemailer.createTransport({
     its rendered as a function so that data can be passed in dynamically from front
     all data is generated from the create event component the user fills in
 */
-const mailOptions = function(email, hostName, eventDate, eventName) {
+const mailOptions = function(email, hostName, eventDate, eventName, userId, eventId) {
  return {
 	from: 'team.eatly@gmail.com',
 	to: email,
-	subject: hostName + ` invited you to a meal with Eatly!`,
+	subject: `${hostName} invited you to a meal with Eatly!`,
 	html:
       `<style>
       @media only screen and (max-width: 620px) {
@@ -106,7 +106,7 @@ const mailOptions = function(email, hostName, eventDate, eventName) {
 
 
               <span class="preheader" style="color: transparent; display: none; height: 0; max-height: 0; max-width: 0; opacity: 0; overflow: hidden; mso-hide: all; visibility: hidden; width: 0;">
-                You've been invited by ` + hostName + ` to collaborate a meal planning on Eatly!</span>
+                You've been invited by ${hostName} to collaborate a meal planning on Eatly!</span>
               
               <table class="main" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; background: #ffffff; border-radius: 4px;">
 
@@ -123,15 +123,15 @@ const mailOptions = function(email, hostName, eventDate, eventName) {
                               You've been invited to 
                               
                               <div style="font-size: 20px; display: inline-block; margin-left: 8px; font-weight: bold;">
-                              ` +eventName+ `!</div> <br>
+                              ${eventName}!</div> <br>
 
                               Hosted by <br> 
                               
                               <div style="font-size: 20px; display: inline-block; margin-left: 8px; margin-right: 8px; font-weight: bold;">
-                              ` +hostName+ `</div> <br> on
+                              ${hostName}</div> <br> on
                               
                               <div style="font-size: 20px; display: inline-block; margin-left: 8px; margin-right: 8px; font-weight: bold;">
-                                ` +eventDate+ `
+                                ${eventDate}
                               </div>... <br><br>
                               
                               "But where?!" you might ask, and funny you should... We don't know yet! That's your job to decide, and all the fun of 
@@ -149,7 +149,7 @@ const mailOptions = function(email, hostName, eventDate, eventName) {
                                   <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: auto;">
                                     <tbody>
                                       <tr>
-                                        <td style="font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: #d30808; border-radius: 5px; text-align: center;"> <a href="http://memory-alpha.wikia.com/wiki/USS_Franklin" target="_blank" style="display: inline-block; color: #ffffff; background-color: #d30808; border: solid 3px #7f0202; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 18px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #7f0202;">Get Started!</a> </td>
+                                        <td style="font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: #d30808; border-radius: 5px; text-align: center;"> <a href="http://localhost:3000/Swipe?userId=${userId}&eventKey=${eventId}" target="_blank" style="display: inline-block; color: #ffffff; background-color: #d30808; border: solid 3px #7f0202; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 18px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #7f0202;">Get Started!</a> </td>
                                       </tr>
                                     </tbody>
                                   </table>
@@ -210,9 +210,8 @@ const mailOptions = function(email, hostName, eventDate, eventName) {
 
 //the actual send email function that takes in same dynamic data
 //also passes along the mailOptions object generated in previous function
-const sendInviteEmail = function(email, hostName, eventDate, eventName) { 
-  transporter.sendMail(mailOptions(email, hostName, eventDate, eventName), function(err, info){
-  	console.log(hostName)
+const sendInviteEmail = function(email, hostName, eventDate, eventName, userId, eventId) { 
+  transporter.sendMail(mailOptions(email, hostName, eventDate, eventName, userId, eventId), function(err, info){
     if(err)
   		console.log(err)
   	else
