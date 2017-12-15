296290faaf63dffc9f5a30bc321cdc8beafbb969
RJ Barnett <RJ.Barnett1@gmail.com>
2017-12-15 11:40:47 -0500

    added decline button with basic functionality to the guest invite email

--- server/controllers/inviteEmailController.js
+++ server/controllers/inviteEmailController.js
@@ -158,6 +158,29 @@ const mailOptions = function(email, hostName, eventDate, eventName) {
                             </tbody>
                           </table>
                           
+                          <p style="font-family: sans-serif; font-weight: normal; margin: 0; Margin-bottom: 15px; display: inline-block;">
+                            Can't make it? Maybe you don't feel like going out? No worries! We'll politely decline on your behalf and keep it secret from the rest of the group. Just click here:
+                          </p>
+
+
+                          <table border="0" cellpadding="0" cellspacing="0" class="btn btn-decline" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; box-sizing: border-box;">
+                            <tbody>
+                              <tr>
+                                <td align="left" style="font-family: sans-serif; font-size: 14px; vertical-align: top; padding-bottom: 15px;">
+                                  <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: auto;">
+                                    <tbody>
+                                      <tr>
+                                        <td style="font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: #d30808; border-radius: 5px; text-align: center;"> <a href="http://personalitybuzz.com/youre-not-antisocial-youre-just-a-homebody/" target="_blank" style="display: inline-block; color: #ffffff; background-color: #d30808; border: solid 3px #7f0202; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 18px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #7f0202;">Decline RSVP</a> </td>
+                                      </tr>
+                                    </tbody>
+                                  </table>
+                                </td>
+                              </tr>
+                            </tbody>
+                          </table>
+
+
+
                           
                         </td>
                       </tr>
