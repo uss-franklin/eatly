355305e4c9f3a23959eaeade3f9fe84433cdffa2
refactoredjello <gil.mandler@gmail.com>
2017-12-21 15:22:58 -0500

    Styling for edit page

--- client/src/components/user/AddGuestForm.jsx
+++ client/src/components/user/AddGuestForm.jsx
@@ -14,19 +14,19 @@ export default class AddGuestForm extends React.Component {
   render() {
     return (
       <div className="guest-email">
-        <label>
-          Guest Email:
+        <label className="label"> Guest Email:</label>
           <input 
+            className="input"
             type="text"
             name="email"
             placeholder="Lookingforfoodtoo@something.com"
             value={this.state.email}
             onChange={this.handleInputChange.bind(this)}
           />
-        </label>
-        <label>
-          Guest Name:
+        
+        <label className="label">Guest Name:</label>
           <input 
+            className="input"
             type="text" 
             name="name"
             placeholder="Guest Name"
@@ -34,7 +34,7 @@ export default class AddGuestForm extends React.Component {
             onChange={this.handleInputChange.bind(this)}
             onBlur={e => this.props.addNewGuestToState(this.state.email, this.state.name, this.props.idx)}
           />
-        </label>
+        
       </div>
     ) 
     
