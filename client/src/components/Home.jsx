8f1c802f01c8bf4b64a49296bada8fe3996c941e
RJ Barnett <RJ.Barnett1@gmail.com>
2017-12-22 09:20:35 -0500

    merge conflicts

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -10,6 +10,37 @@ const Home = () => (
             <Link to="/inputForm" style={{ textDecoration: 'none'}}>Plan a Meal</Link>
           </button>  
       </div>
+    
+      <div className="aboutMeSectionContainer"> 	
+      
+      	<div className="generalInfoAppTextContainer">
+      		<h1 className="generalInfoAppTextHeader">
+      		</h1>
+
+      		<p className="generalInfoAppText">
+      		</p>
+      	</div>
+      
+      	<div className="basicFunctionalityGuidelineTextContainer">
+      		<h1 className="basicFunctionalityGuidelineTextHeader">
+      		</h1>
+
+      		<p className="basicFunctionalityGuidelineText">
+      		</p>
+      	</div>
+
+      	<div className="magicOfEatlyTextContainer">
+      		<h1 className="magicOfEatlyTextHeader">
+      		</h1>
+
+      		<p className="magicOfEatlyText">
+      		</p>
+      	</div>
+      
+      </div>
+
+
+
     </div>
 )
 
